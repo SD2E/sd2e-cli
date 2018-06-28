2fa9da79d74a330630876c0955366126d221d789
alejandrox1 <alarcj137@gmail.com>
2018-06-28 20:09:12 +0000

    build/customize.sh added documentation
    Signed-off-by: alejandrox1 <alarcj137@gmail.com>

--- build/customize.sh
+++ build/customize.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
+#
+# customize.sh
+#
+# Package executables and scripts from submodules and this repo into a $TARGET
+# directory for it to be tar'd and packaged as a new release.
 
-# $(OBJ)
+# $TARGET corresponds to the final location of the executables and scripts to
+# be packaged as a release ($OBJ in the main Makefile).
 TARGET=$1
 
 if [ ! -d "${TARGET}" ]
