aebcf7f5977fc51b637a02e00942ac1080ab3a43
alejandrox1 <alarcj137@gmail.com>
2018-06-29 17:39:58 +0000

    build/docker.sh cleaner implementation and documentation of script
    Signed-off-by: alejandrox1 <alarcj137@gmail.com>

--- build/docker.sh
+++ build/docker.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+#
+# docker.sh
+#
+# By default, this script will build ($COMMAND "build") a docker container 
+# image with sd2e-cli executables. Other options are "release", and "clean".
+#
+# Environment variables specified in configuration.rc (root of dev tree and
+# sourced by Makefile): $TENANT_DOCKER_ORG, $TENANT_DOCKERFILE.
+#
+# Usage:
+#   docker.sh sd2e-cloud-cli v1.0.0 build
+#
 
 IMAGENAME=$1
 SDKVERSION=$2
@@ -16,35 +28,39 @@ function die {
 }
 
 
-
+# Build conatiner image with sd2e-cli.
 if [ "$COMMAND" == 'build' ];
 then
+    # Tag image with $SDKVERSION.
+    docker build --no-cache --force-rm \
+        -f ${TENANT_DOCKERFILE} \
+        -t ${TENANT_DOCKER_ORG}/${IMAGENAME}:${SDKVERSION} . || \
+        die "Error on build. Exiting."
 
-docker build --rm=true -t ${TENANT_DOCKER_ORG}/${IMAGENAME}:${SDKVERSION} -f ${TENANT_DOCKERFILE} .
-if [ $? -ne 0 ] ; then die "Error on build. Exiting." ; fi
-
-IMAGEID=`docker images -q  ${TENANT_DOCKER_ORG}/${IMAGENAME}:${SDKVERSION}`
-if [ $? -ne 0 ] ; then die "Can't find image ${TENANT_DOCKER_ORG}/${IMAGENAME}:${SDKVERSION}. Exiting." ; fi
-
-docker tag ${IMAGEID} ${TENANT_DOCKER_ORG}/${IMAGENAME}:latest
-if [ $? -ne 0 ] ; then die "Error tagging with 'latest'. Exiting." ; fi
-
+    # Tag image with "latest".
+    IMAGEID=`docker images -q  ${TENANT_DOCKER_ORG}/${IMAGENAME}:${SDKVERSION}` 
+    test -z $IMAGEID && die "Can't find image ${TENANT_DOCKER_ORG}/${IMAGENAME}:${SDKVERSION}."
+        
+    docker tag ${IMAGEID} ${TENANT_DOCKER_ORG}/${IMAGENAME}:latest
 fi
 
 
+
+# Push conatiner image with sd2e-cli.
 if [ "$COMMAND" == 'release' ];
 then
-
-docker push ${TENANT_DOCKER_ORG}/${IMAGENAME}:${SDKVERSION} && docker push ${TENANT_DOCKER_ORG}/${IMAGENAME}:latest
-
-if [ $? -ne 0 ] ; then die "Error pushing to Docker Hub. Exiting." ; fi
+    docker push ${TENANT_DOCKER_ORG}/${IMAGENAME}:${SDKVERSION} || \
+        die "Error pushing ${TENANT_DOCKER_ORG}/${IMAGENAME}:${SDKVERSION} to Docker Hub."
+        
+    docker push ${TENANT_DOCKER_ORG}/${IMAGENAME}:latest || \
+        die "Error pushing ${TENANT_DOCKER_ORG}/${IMAGENAME}:latest to Docker Hub."
 fi
 
 
+
+# Remove conatiner image.
 if [ "$COMMAND" == 'clean' ];
 then
-
-docker rmi -f ${TENANT_DOCKER_ORG}/${IMAGENAME}:${SDKVERSION} && docker rmi -f ${TENANT_DOCKER_ORG}/${IMAGENAME}:latest
-
-if [ $? -ne 0 ] ; then die "Error deleting local images. Exiting." ; fi
+    docker rmi -f ${TENANT_DOCKER_ORG}/${IMAGENAME}:${SDKVERSION}
+    docker rmi -f ${TENANT_DOCKER_ORG}/${IMAGENAME}:latest
 fi
